e5188d7bafdc2df471b83c2c6ab45ac0f94d95b8
andylinteau <andy.linteau@gmail.com>
2014-12-02 03:07:23 -0500

    add random color functionality

--- sketch.js
+++ sketch.js
@@ -11,12 +11,18 @@ $(document).ready(function(){
 		$(this).addClass('red');
 	});
 	
+	$("button.random").on('mouseenter', function(){
+		var rgb1 = Math.floor(Math.random()*256);
+		var rgb2 =  Math.floor(Math.random()*256); 
+		var rgb3 =  Math.floor(Math.random()*256); 
+		$(this).css({'background' : "rgb(" + rgb1 +"," + rgb2 +"," + rgb3 });
+	});
 });
 
 
-//functions for the game
+//functions for the random color game
 
-function createGrid(gridSize){
+function createRandomColorGrid(gridSize){
 	totalGrid = gridSize*gridSize;
 	i = 0
 		while (i < totalGrid){
@@ -31,18 +37,49 @@ function createGrid(gridSize){
 	$("div").find('.square').css({'width': width+"px", 'height': height + "px"});
 
 	$(".square").on('mouseenter', function(){
-		$(this).addClass('red');
+		var rgb1 = Math.floor(Math.random()*256);
+		var rgb2 =  Math.floor(Math.random()*256); 
+		var rgb3 =  Math.floor(Math.random()*256); 
+		$(this).css({'background' : "rgb(" + rgb1 +"," + rgb2 +"," + rgb3 });
 	});
 }
 
 
-function newGame(){
+function randomColor(){
 					 $(".removable").remove();
 				 grid = prompt();
 
-				 createGrid(grid);
+				 createRandomColorGrid(grid);
 				 console.log(grid);
+	}
 
 
+/// functions for the regular game
+
+function createRegGrid(gridSize){
+	totalGrid = gridSize*gridSize;
+	i = 0
+		while (i < totalGrid){
+		$('#container').append("<div class='square removable'></div>");
+	
+		i = i + 1
+	};
+
+	width = 800/gridSize;
+	height = 800/gridSize;
+
+	$("div").find('.square').css({'width': width+"px", 'height': height + "px"});
+
+	$(".square").on('mouseenter', function(){
+		$(this).css({'background' : "lightblue"});
+	});
+}
+
+	function regGame(){
+					 $(".removable").remove();
+				 grid = prompt();
+
+				 createRegGrid(grid);
+				 console.log(grid);
 	}
 
