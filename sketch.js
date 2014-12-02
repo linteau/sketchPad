d8d9a67d0fef5f9179471a9ee9e39b6c5fb0d8ab
andylinteau <andy.linteau@gmail.com>
2014-12-02 02:26:08 -0500

    style grid and finish project phase 1

--- sketch.js
+++ sketch.js
@@ -1,8 +1,48 @@
 $(document).ready(function(){
+
+
 	x = 0
 	while (x < 16){
-		$('#container').append("<div></div>");
+		$('#container').append("<div class='square removable'></div>");
 		x = x + 1
 	};
+
+	$('.square').on('mouseenter', function(){
+		$(this).addClass('red');
+	});
 	
 });
+
+
+//functions for the game
+
+function createGrid(gridSize){
+	totalGrid = gridSize*gridSize;
+	i = 0
+		while (i < totalGrid){
+		$('#container').append("<div class='square removable'></div>");
+	
+		i = i + 1
+	};
+
+	width = 800/gridSize;
+	height = 800/gridSize;
+
+	$("div").find('.square').css({'width': width+"px", 'height': height + "px"});
+
+	$(".square").on('mouseenter', function(){
+		$(this).addClass('red');
+	});
+}
+
+
+function newGame(){
+					 $(".removable").remove();
+				 grid = prompt();
+
+				 createGrid(grid);
+				 console.log(grid);
+
+
+	}
+
